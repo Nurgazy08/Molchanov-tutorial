41a9a279d4458c252ebce3e25abbe6dbcc3ca208
nurgazy08 <nurgazy.mamashev@gmail.com>
2020-11-09 15:48:40 +0600

    lesson 8

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import View
 from .models import Post, Tag
-from .utils import ObjectDetailMixin
-from .forms import TagForm
+from .utils import *
+from .forms import TagForm, PostForm
 
 # Create your views here.
 
@@ -19,24 +19,24 @@ class PostDetail(ObjectDetailMixin, View):
 
 
 
+class PostCreate(ObjectCreateMixin, View):
+
+    model_form = PostForm
+    template = 'blog/post_create_form.html'
+
+
+
+
+
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
 
 
-class TagCreate(View):
-    def get(self, request):
-        form = TagForm()
-        return render(request, 'blog/tag_create.html', context={'form': form})
+class TagCreate(ObjectCreateMixin, View):
 
-    def post(self, request):
-        bound_from = TagForm(request.POST)
-
-        if bound_from.is_valid():
-            new_tag = bound_form.save()
-            return redirect(new_tag)
-
-        return render(request, 'blog/tag_create.html', context={'form': bound_form})
+    model_form = TagForm
+    template = 'blog/tag_create.html'
 
 
 def tags_list(request):
