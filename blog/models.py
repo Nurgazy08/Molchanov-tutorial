fc221fb4d26cd2af8b024057b8df471a025885db
nurgazy08 <nurgazy.mamashev@gmail.com>
2020-11-09 16:26:29 +0600

    lesson 9

--- blog/models.py
+++ blog/models.py
@@ -22,11 +22,13 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
 
+    def get_update_url(self):
+        return reverse('post_update_url', kwargs={'slug': self.slug})
+
     def save(self, *args, **kwargs):
         if not self.id:
             self.slug = gen_slug(self.title)
         super().save(*args, **kwargs)
-
 
     def __str__(self):
         return self.title
@@ -37,9 +39,11 @@ class Tag(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
 
-
     def get_absolute_url(self):
         return reverse('tag_detail_url', kwargs={'slug': self.slug})
+
+    def get_update_url(self):
+        return reverse('tag_update_url', kwargs={'slug': self.slug})
 
     def __str__(self):
         return '{}'.format(self.title)
--- blog/views.py
+++ blog/views.py
@@ -11,13 +11,9 @@ def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/index.html', context={'posts': posts})
 
-
-
 class PostDetail(ObjectDetailMixin, View):
     model = Post
     template= 'blog/post_detail.html'
-
-
 
 class PostCreate(ObjectCreateMixin, View):
 
@@ -25,18 +21,38 @@ class PostCreate(ObjectCreateMixin, View):
     template = 'blog/post_create_form.html'
 
 
-
+class PostUpdate(ObjectUpdateMixin, View):
+    model = Post
+    model_form = PostForm
+    template = 'blog/post_update_form.html'
 
 
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
 
-
 class TagCreate(ObjectCreateMixin, View):
-
     model_form = TagForm
     template = 'blog/tag_create.html'
+
+class TagUpdate(ObjectUpdateMixin, View):
+    model = Tag
+    model_form = TagForm
+    template = 'blog/tag_update_form.html'
+    # def get(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     bound_form = TagForm(instance=tag)
+    #     return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
+
+    # def post(self, request, slug):
+    #     tag = Tag.objects.get(slug__iexact=slug)
+    #     bound_form = TagForm(request.POST, instance=tag)
+
+    #     if bound_form.is_valid():
+    #         new_tag = bound_form.save()
+    #         return redirect(new_tag)
+    #     return render(request, 'blog/tag_update_form', context={'form': bound_form, 'tag': tag})
+
 
 
 def tags_list(request):
