f3706fe63994e7d90c9725f688986bc03e2925e1
nurgazy08 <nurgazy.mamashev@gmail.com>
2020-11-10 13:08:17 +0600

    lesson 11

--- blog/utils.py
+++ blog/utils.py
@@ -8,7 +8,7 @@ class ObjectDetailMixin:
     template = None
     def get(self, request, slug):
         obj = get_object_or_404(self.model, slug__iexact=slug)
-        return render(request, self.template, context={self.model.__name__.lower(): obj})
+        return render(request, self.template, context={self.model.__name__.lower(): obj, 'admin_object': obj, 'detail': True})
 
 
 
@@ -22,7 +22,7 @@ class ObjectCreateMixin:
         return render(request, self.template, context={'form': form})
 
     def post(self, request):
-        bound_from = self.form_model(request.POST)
+        bound_form = self.model_form(request.POST)
 
         if bound_form.is_valid():
             new_obj = bound_form.save()
