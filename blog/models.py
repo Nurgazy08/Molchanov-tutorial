22844bceb3900aee2597d4a32f8d4b2b130d7abc
nurgazy08 <nurgazy.mamashev@gmail.com>
2020-11-10 13:46:27 +0600

    lesson 12

--- blog/models.py
+++ blog/models.py
@@ -1,23 +1,20 @@
 from django.db import models
 from django.shortcuts import reverse
+
 from django.utils.text import slugify
 from time import time
-# Create your models here.
 
 def gen_slug(s):
     new_slug = slugify(s, allow_unicode=True)
     return new_slug + '-' + str(int(time()))
 
-
-
-
-
 class Post(models.Model):
-    title = models.CharField(max_length=150, db_index=True)
+    title = models.CharField(max_length=150, db_index= True)
     slug = models.SlugField(max_length=150, blank=True, unique=True)
     body = models.TextField(blank=True, db_index=True)
-    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
     date_pub = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
+
 
     def get_absolute_url(self):
         return reverse('post_detail_url', kwargs={'slug': self.slug})
@@ -28,7 +25,6 @@ class Post(models.Model):
     def get_delete_url(self):
         return reverse('post_delete_url', kwargs={'slug': self.slug})
 
-
     def save(self, *args, **kwargs):
         if not self.id:
             self.slug = gen_slug(self.title)
@@ -37,7 +33,8 @@ class Post(models.Model):
     def __str__(self):
         return self.title
 
-
+    class Meta:
+        ordering = ['-date_pub']
 
 class Tag(models.Model):
     title = models.CharField(max_length=50)
@@ -48,9 +45,12 @@ class Tag(models.Model):
 
     def get_update_url(self):
         return reverse('tag_update_url', kwargs={'slug': self.slug})
-
+    
     def get_delete_url(self):
         return reverse('tag_delete_url', kwargs={'slug': self.slug})
 
     def __str__(self):
         return '{}'.format(self.title)
+
+    class Meta:
+        ordering = ['title']
--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,55 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.urls import reverse
-from django.views.generic import View
 from .models import Post, Tag
-from .utils import *
+from django.views.generic import View
+from .utils import  *
 from .forms import TagForm, PostForm
+from django.urls import reverse
 from django.contrib.auth.mixins import LoginRequiredMixin
-# Create your views here.
+from django.core.paginator import Paginator
 
 
 def posts_list(request):
     posts = Post.objects.all()
-    return render(request, 'blog/index.html', context={'posts': posts})
+    paginator = Paginator(posts, 2)
+
+    page_number = request.GET.get('page', 1)
+    page = paginator.get_page(page_number)
+
+
+    is_paginated = page.has_other_pages()
+
+    if page.has_previous():
+        prev_url = '?page={}'.format(page.previous_page_number())
+    else:
+        prev_url = ''
+
+    if page.has_next():
+        next_url = '?page={}'.format(page.next_page_number())
+    else:
+        next_url = ''
+    
+    context = {
+        'page_object': page,
+        'is_paginated': is_paginated,
+        'next_url': next_url,
+        'prev_url': prev_url,
+    } 
+
+    return render(request, 'blog/index.html', context=context)
 
 class PostDetail(ObjectDetailMixin, View):
     model = Post
-    template= 'blog/post_detail.html'
+    template = 'blog/post_detail.html'
 
 class PostCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     model_form = PostForm
     template = 'blog/post_create_form.html'
     raise_exception = True
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    return render(request, 'blog/tags_list.html', context={'tags': tags})
 
 
 class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
@@ -28,21 +58,17 @@ class PostUpdate(LoginRequiredMixin, ObjectUpdateMixin, View):
     template = 'blog/post_update_form.html'
     raise_exception = True
 
-
 class PostDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     model = Post
     template = 'blog/post_delete_form.html'
-    redirect_url = 'post_list_url'
+    redirect_url = 'posts_list_url'
     raise_exception = True
-
-
-
-
 
 
 class TagDetail(ObjectDetailMixin, View):
     model = Tag
     template = 'blog/tag_detail.html'
+
 
 class TagCreate(LoginRequiredMixin, ObjectCreateMixin, View):
     model_form = TagForm
@@ -61,9 +87,3 @@ class TagDelete(LoginRequiredMixin, ObjectDeleteMixin, View):
     template = 'blog/tag_delete_form.html'
     redirect_url = 'tags_list_url'
     raise_exception = True
-
-
-def tags_list(request):
-    tags = Tag.objects.all()
-    return render(request, 'blog/tags_list.html', context={'tags': tags})
-
